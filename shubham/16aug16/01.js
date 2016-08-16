06ff5b1de10f669dc32f4b5484f37cccb054b1c6
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2016-08-16 20:39:57 +0530

    COrrected in 01.js

--- shubham/16aug16/01.js
+++ shubham/16aug16/01.js
@@ -3,10 +3,7 @@ var tshirt = document.getElementById('tshirt');
 var mug = document.getElementById('mug');
 var pencil = document.getElementById('pencil');
 var sticker = document.getElementById('sticker');
-var numtshirt = document.getElementById('quantity');
-var quantity = Number(numtshirt.options[numtshirt.selectedIndex].value);
 var c = document.getElementById('color');
-var color = c.options[c.selectedIndex].text;
 var s = document.getElementById('size');
 var size = s.options[s.selectedIndex].text;
 var area = document.getElementById('total');
@@ -14,6 +11,9 @@ var form = document.getElementById('form');
 
 
 function validate(){
+	var total = 0;
+	var x = document.getElementById("quantity").value;
+
 	if(mug.checked){
 		total+=Number(4.99);
 	}
@@ -24,8 +24,7 @@ function validate(){
 		total+=Number(1.99);
 	}
 	if(tshirt.checked){
-		/*total+= Number(9.99)*Number((quantity));
-		alert(Number(quantity));*/
+		total+= Number(9.99)*Number(x);
 	}
 	area.innerHTML = 'Total: ' + '$' + total;
 }
