39f96ef734b811bb2c1e0bb4e255c3d5d19671c1
aman_saha <aman.saha2015@vit.ac.in>
2016-09-07 19:23:53 +0530

    assign done

--- aman/lulu/vote.php
+++ aman/lulu/vote.php
@@ -1,58 +1,3 @@
-
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>Js test</title>
-	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
-	<link rel="stylesheet" type="text/css" href="01.css"/>
-</head>
-<body>
-	<div class="jumbotron">
-		<h1>Voting system</h1>	
-	</div>
-	<div class="container">
-		<div class="col-md-12 col-sm-12">
-			Enter the number of candidates:
-			<input type="text" class="form-control" id="cand" name = "candidates"/>
-			<br />
-			Enter the number of voters:
-			<input type="text" class="form-control" id="voters" name = "voter"/>
-			<br />
-			<div align="center">					
-				<input type = "submit" class="btn btn-danger" id="intbtn" onclick="create()" name="submit">	
-			</div>
-			<br />			
-			<div class="jumbotron" id="space">
-				<h4 id="voter-info"></h4>
-			</div>	
-			<div align="center">
-				<button class="btn btn-success" name = "done">Done</button>
-			</div>			
-		</div>		
-	</div>
-	<body>
-		<script>
-			var c = 0,v = 0;
-			var i = 0;
-			function create(){
-				c = Number(document.getElementById('cand').value);
-				v = Number(document.getElementById('voters').value);
-				var info = document.getElementById('voter-info');
-				info.innerHTML = 'Enter the rankings below for the candidates from ' + '1 to '+ c;
-				for(var i=0;i<c;i++){
-					var a = document.createElement('input');
-					a.setAttribute('type','text');
-					a.className = "vote";
-					a.setAttribute('placeholder','Candidate ' + String(Number(i)+1));
-					document.getElementById('space').appendChild(a);
-					document.getElementById('space').appendChild(document.createElement('br'));
-					document.getElementById('space').appendChild(document.createElement('br'));
-				}
-			}
-		</script>
-		</html>
-
 <?php
     $DB_SERVER="localhost";
     $DB_USER="root";
@@ -73,23 +18,66 @@
         die("Database selection failed.....: ");
     }
 ?>
-<?php
-	if(isset($_POST['submit']))
-	{
-		echo "sdds";
-		$candidates = $_POST['candidates'];
-		$voters = $_POST['voter'];
-		for($i=1;$i<$voters;$i++)
-		{
-			$candidates.=$i;
-			$add = "ALTER TABLE inventory ADD '" .$candidates."' TINYINT NULL ";
-			$result = mysqli_query($connection,$add);
-			if($result)
-			{
-				echo "good";
-			}
-			else
-				echo "bad";
-		}
-	}
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Js test</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+	<link rel="stylesheet" type="text/css" href="01.css"/>
+</head>
+<body>
+	<div class="jumbotron">
+		<h1>Voting system</h1>	
+	</div>
+	<form method = "POST" action = "vote.php">
+		<div class="container">
+			<div class="col-md-12 col-sm-12">
+				Enter the number of candidates:
+				<input type="text" class="form-control" id="cand" name = "candidates"/>
+				<br />
+				Enter the number of voters:
+				<input type="text" class="form-control" id="voters" name = "voter"/>
+				<br />
+				<div align="center">					
+					<input type = "submit" class="btn btn-danger" id="intbtn" onclick="create()" name="submit">	
+				</div>
+				<br />			
+				<div class="jumbotron" id="space">
+					<h4 id="voter-info">
+						<?php
+							$candidate=0;
+							$voters=0;
+							if(isset($_POST['submit']))
+							{
+								$candidate = $_POST['candidates'];
+								$str = "candidate";
+								$voters = $_POST['voter'];
+								$output="";
+								$output .= '<div class="table-responsive">  
+							                    <table class="table table bordered">';
+								for($i=1;$i<=$voters;$i++)
+								{
+									$str.=$i;
+									/*$add = "ALTER TABLE votes ADD ".$str." INT ";
+									$result = mysqli_query($connection,$add);*/
+									$output.='<td>
+												<tr>Candidate <input type="text" name = "'.$str.'"></tr>
+											  </td>
+											  </table>
+											  </div>';
+									$str="candidate";
+								}
+								echo "$output";
+							}
+						?>
+					</h4>
+				</div>	
+				<div align="center">
+					<button class="btn btn-success" name = "done">Done</button>
+				</div>			
+			</div>		
+		</div>
+	</form>
+	<body>
+</html>
