ff5e30022b32944bc4bf57d8c5a35dcdf3930156
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2016-10-18 18:06:13 +0530

    All uptodate

---
+++ shubham/11oct16/bookShop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+class Book{
+    String author,title,publisher,
+    int id,numOfCopies;
+    double price;
+}
+
+public class bookShop extends Book{
+
+}
--- shubham/lab10/update.php
+++ shubham/lab10/update.php
@@ -1,130 +1,45 @@
 <?php
 $servername = "localhost";
-
-
 $username = "root";
-
-
 $password = "";
-
-
 $dbname = "myDB";
-
-
-
-
 // Create connection
-
-
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-
-
-
-
 // Check connection
-
-
-if ($conn->connect_error)
-
-
-{
-
-
+if ($conn->connect_error){
 die("Connection failed: " . $conn->connect_error);
-
-
 }
-else
-{
+else{
 $sql = "UPDATE student SET name='d' WHERE gpa=5";
-
-
-
-
 if ($conn->query($sql) === TRUE) {
-
-
 echo "Record updated successfully <br>";
-
-
 } else {
-
-
 echo "Error updating record: " . $conn->error;
-
-
 }
-
-
 }
-
-
 $sql = "SELECT FirstName, LastName, Age from Persons";
-
-
 $result = $conn->query($sql);
-
-
 if ($result->num_rows > 0) {
-
-
-
-
 Update
-
-
 // output data of each row
-
-
 echo "First Name LastName Age ";
-
-
 while($row = $result->fetch_assoc()) {
-
-
 echo "<br>" . $row["FirstName"]. " " . $row["LastName"]. " " . $row["Age"];
-
-
 }
-
-
 } else {
-
-
 echo "0 results";
-
-
 }
 $sql="select regno,name,gpa,email from STUDENT ";
 $result=$conn->query($sql);
 if ($result->num_rows > 0) {
-
-
 // output data of each row
-
-
 echo "First Name LastName Age ";
-
-
 while($row = $result->fetch_assoc()) {
-
-
 echo "<br>" . $row["regno"]. " " . $row["name"]. " " .
 $row["gpa"]." ". $row["email"];
-
-
 }
-
-
 } else {
-
-
 echo "0 results";
-
-
 }
-
-
 $conn->close();
 ?>
